5437c44a2bc98126b0468ee593a3f25d923a17ea
davidlpc1 <davipadro76@gmail.com>
2020-10-19 19:33:53 -0300

    Aviso Para baixar app mobile caso site acessado pelo celular

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import { FiArrowRight } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+import Popup from 'reactjs-popup';
 
+import styled from 'styled-components'
 import '../styles/pages/landing.css'
 import logoImg from '../images/logo.svg'
 
 function Landing (){
+    const isMobile = navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    if( isMobile ){
+      return(
+        <Popup
+          trigger={open => (
+            <div id="page-popup">
+              <ButtonPopup >{open ? (
+                <>
+                  <span> Você está na versão Web ,porém num <strong>dispositivo Mobile</strong> </span>
+                  <span> Em breve <strong>app</strong> disponível </span>
+                  <Link to='/app'>
+                    Ir ao mapa
+                  </Link>
+                </>
+               ) : 'Clique aqui'}</ButtonPopup>
+            </div>
+          )}
+          position="bottom center"
+          closeOnDocumentClick
+        >
+          <div id="page-popup">
+
+          </div>
+        </Popup>
+      )
+    }
     return (
         <div id="page-landing">
         <div className="content-wrapper">
@@ -29,5 +63,13 @@ function Landing (){
       </div>
     );
 }
+
+const ButtonPopup = styled.button`
+  width:285px;
+  height:82px;
+  background: #ffd666;
+  border:none;
+
+`
 
 export default Landing;
